665d3d8b53ef37ed3c41cbbad9571a451a81fa9d
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-11-24 16:18:13 -0300

    ok
    OKs

--- Repository/HviRepository.cs
+++ Repository/HviRepository.cs
@@ -17,34 +17,29 @@ namespace FileManagerAPI.Repository
             try
             {
 
-                using (SqlConnection cn = new(sqlString.GetCadenaSQL()))
-                {
-                    cn.Open();
-                    var cmd = new SqlCommand("SP_GetData", cn);
 
                     model.ForEach(x =>
                     {
-                        //var UHML = x.Uhml;
-                        //var UI = x.Ui;
-                        //var STRENGTH = x.Strength;
-                        //var SFI = x.Sfi;
-                        //var MIC = x.Mic;
-                        //var COLORGRADE = x.Colorgrade;
-                        //var TRASHID = x.Trashid;
+                        using (SqlConnection cn = new(sqlString.GetCadenaSQL()))
+                        {
+                            cn.Open();
+                            var cmd = new SqlCommand("SP_GetData", cn);
 
-                        cmd.Parameters.AddWithValue("UHML", x.Uhml);
-                        cmd.Parameters.AddWithValue("UI", x.Ui);
-                        cmd.Parameters.AddWithValue("STRENGTH", x.Strength);
-                        cmd.Parameters.AddWithValue("SFI", x.Sfi);
-                        cmd.Parameters.AddWithValue("MIC", x.Mic);
-                        cmd.Parameters.AddWithValue("COLORGRADE", x.Colorgrade);
-                        cmd.Parameters.AddWithValue("TRASHID", x.Trashid);
+                            cmd.Parameters.AddWithValue("UHML", x.Uhml);
+                            cmd.Parameters.AddWithValue("UI", x.Ui);
+                            cmd.Parameters.AddWithValue("STRENGTH", x.Strength);
+                            cmd.Parameters.AddWithValue("SFI", x.Sfi);
+                            cmd.Parameters.AddWithValue("MIC", x.Mic);
+                            cmd.Parameters.AddWithValue("COLORGRADE", x.Colorgrade);
+                            cmd.Parameters.AddWithValue("TRASHID", x.Trashid);
+
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.ExecuteReader();
+                        }
+
                     });
-
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.ExecuteReader();
-
-
+                    
+                return "se ha guardado con exito";
 
                     //using (var rd = cmd.ExecuteReader())
                     //{
@@ -57,9 +52,6 @@ namespace FileManagerAPI.Repository
                     //        model.SFI = (decimal)rd["SFI"];
                     //    }
                     //}
-
-                    return "se ha guardado con exito";
-                }
             }
             catch (Exception e)
             {
